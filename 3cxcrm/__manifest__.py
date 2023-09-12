5a6a01d8a13490435aa90c35fbc7da43365bc6b3
Robby <crotti.roberto@gmail.com>
2023-09-12 15:45:38 +0200

    first commit to 16.0

--- 3cxcrm/__manifest__.py
+++ 3cxcrm/__manifest__.py
@@ -15,7 +15,7 @@
     
     # for the full list
     'category': 'Productivity',
-    'version': '15.0.1',
+    'version': '16.0.1',
 
     # any module necessary for this one to work correctly
     'depends': ['base','crm', 'partner_firstname'],
