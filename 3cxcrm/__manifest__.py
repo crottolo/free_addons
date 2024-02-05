bfa5e7790a7638e0f87e75afc1b4647a6fa6db06
Robby <crotti.roberto@gmail.com>
2024-02-05 17:09:45 +0100

    removed first name module 3cx free

--- 3cxcrm/__manifest__.py
+++ 3cxcrm/__manifest__.py
@@ -29,7 +29,6 @@
     ],
     # only loaded in demonstration mode
     'demo': [
-        'demo/demo.xml',
     ],
     
 }
--- 3cxcrm/controllers/controllers.py
+++ 3cxcrm/controllers/controllers.py
@@ -103,48 +103,4 @@ class Odoo3cxCrm(http.Controller):
         return BadRequest('ApiKey not set')
         
 
-    ####odoo crm.lead search lead
-    #query = request.env['crm.lead'].with_user(1).search([('phone_mobile_search','ilike', '39358')])
-
-
-
-        # dato = data.get('messages','')
-        # print(dato)
-        # for msg in dato:
-        #     date = datetime.datetime.fromtimestamp( msg.get('date','') ) 
-        #     request.env['wa.message'].sudo().create({
-        #         'id_mp': msg.get('id',''),
-        #         'messenger': msg.get('messenger',''),
-        #         'usernumber': msg.get('usernumber',''),
-        #         'date': date,
-        #         'attachment': msg.get('attachment',''),
-        #         'attachment_type': msg.get('attachment_type',''),
-        #         'text': msg.get('text',''),
-        #         'welcome': msg.get('welcome',''),
-        #         'userstatus': msg.get('userstatus',''),
-        #         'chatid': msg.get('chat_id',''),
-        #         'is_retry': msg.get('is_retry',''),
-        #         'ticket_id': msg.get('ticket_id',''),
-        #         'ticket_status': msg.get('ticket_status',''),
-        #         'agent_id': msg.get('agent_id',''),
-        #         'inbound': True,
-        #         'outbound': False
-                
-                
-        #     })
-        # sender =  msg.get('usernumber','')
-        # exist_channel = request.env['mail.channel'].sudo().search([('sender', '=', sender)])
-        # print('exist_channel',exist_channel)
-        # if exist_channel:
-        #     self.send_to_channel( msg.get('text',''), exist_channel.id)
-        # else:
-        #     aaa = self.search_sender(sender)
-            
-            
-        #     new_channel = self.channel_create( aaa+" "+sender, sender, privacy='public')
-            
-        #     self.send_to_channel( msg.get('text',''), new_channel.get('id',''))
-        #print("response", response)
-
-
    
